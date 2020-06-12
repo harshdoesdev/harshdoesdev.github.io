229a3d53fd21deb120220e50b2cd4992e5294cd1
rwbeast <64768386+rwbeast@users.noreply.github.com>
2020-06-12 22:00:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,3 +14,21 @@ function handle() {
 
 window.addEventListener('load', handle);
 window.addEventListener('hashchange', handle);
+
+const sections = document.querySelectorAll('section');
+
+  window.addEventListener('scroll', () => {
+
+    sections.forEach(section => {
+
+      if(window.scrollY >= section.offsetY) {
+
+        document.querySelector('a.active').classList.remove('active');
+
+        document.querySelector('[href="#' + section.id + '"]').classList.add('active');
+
+      }
+
+    });
+
+  });
