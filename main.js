a2303b8e59b22430f039cd96264c6f993c03caa6
rwbeast <64768386+rwbeast@users.noreply.github.com>
2020-06-12 23:15:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const sections = document.querySelectorAll('section');
 
+const header = document.querySelector('header');
+
 window.addEventListener('scroll', () => {
 
-  const sy = window.scrollY;
+  const sy = header.offsetTop + header.offsetHeight;
     
   sections.forEach(section => {
 
