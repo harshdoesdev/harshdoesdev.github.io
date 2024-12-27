bfb2cc7bd82d936bb05e3a12b2e8d2dacda84296
Harsh Singh <harshdoesdev@gmail.com>
2024-12-28 00:19:03 +0530

    update about.js and some minor fixes

---
+++ resources/about.js
@@ -0,0 +1,22 @@
+export const fakeFiles = ['portfolio.txt', '.config', '.env', 'main.js'];
+
+export const aboutMe = [
+    { text: "Harsh Singh", bold: true, fontSize: '2rem' },
+    "I am a software engineer.",
+    "Email: harshdoesdev[at]gmail.com",
+    "Location: India",
+    { type: 'link', label: 'GitHub', href: 'https://github.com/harshdoesdev', text: "@harshdoesdev" },
+    { type: 'link', label: 'LinkedIn', href: 'https://linkedin.com/in/harshsingh-in', text: "@harshsingh-in" },
+    { type: 'link', label: 'X', href: 'https://x.com/harshthedev', text: "@harshthedev" },
+    "",
+    'Enter "projects" to see a list of the projects I have contributed to.',
+];
+
+export const projects = [
+    { type: 'link', text: 'fastn - an easy-to-learn open-source programming language written in Rust.', link: 'https://fastn.com' },
+    { type: 'link', text: 'RayQL - A schema definition and query language for SQLite.', link: 'https://rayql.com' },
+    { type: 'link', text: 'Shuru - A task runner and version manager for Node.js and Python, written in Rust.', link: 'https://rayql.com' },
+    { type: 'link', text: 'Bottlecap.js - A 2D Game Framework written in Javascript', link: 'https://bottlecap.js.org' },
+    { type: 'link', text: 'Squareroot -  A Short Video Platform for Math Enthusiasts.', link: 'https://sqrroot.web.app' },
+    { type: 'link', text: 'PixelGrid - Pixel Art Editor', link: 'https://pixel-grid-app.surge.sh' },
+];
